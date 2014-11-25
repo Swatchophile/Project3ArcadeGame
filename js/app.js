f8a9bbc5f1bc16a3755a9560e722cf3f11f23829
Swatchophile <sandhyashadangi@gmail.com>
2014-11-25 00:30:59 -0800

    Update app.js

--- js/app.js
+++ js/app.js
@@ -3,7 +3,7 @@
  * This class requires an update(), render() and
  * a handleInput() method.
  * @constructor
- * /
+ */
 var Player = function () {
 	this.sprite = 'images/char-boy.png';
 	this.x = 205;
@@ -12,8 +12,8 @@ var Player = function () {
 };
 
 Player.prototype.update = function(dt) {
-	/** checks for player-enemy collisions and updates player position on collision */
 	for(var i = 0, numOfEnemies = allEnemies.length; i < numOfEnemies; i++) {
+		/** Checks for player-enemy collisions and updates player position on collision */
 		if(player.x <= (allEnemies[i].x + 55) && 
 		allEnemies[i].x <= (player.x + 55) && 
 		player.y <= (allEnemies[i].y + 50) && 
@@ -21,7 +21,7 @@ Player.prototype.update = function(dt) {
 			player.x = 205;
 			player.y = 380;
 		}
-		//checks if player is touching water blocks, and resets player's position if true	
+		/** Checks if player is touching water blocks, and resets player's position if true */	
 		if(player.y < 60) {
 			player.x = 205;
 			player.y = 380;
@@ -33,7 +33,7 @@ Player.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
 
-/** handles player movement direction and speed */
+/** Handles player movement direction and speed */
 Player.prototype.handleInput = function(_key) {
 	switch(_key) {
 		 case 'left':
@@ -50,7 +50,7 @@ Player.prototype.handleInput = function(_key) {
 			break;
 	}
 		
-	/** conditions to ensure that player cannot leave canvas (game environment) boundaries */
+	/** Conditions to ensure that player cannot leave canvas (game environment) boundaries */
 	if(player.x < 0) {
 		player.x = 0;
 	}
@@ -65,7 +65,7 @@ Player.prototype.handleInput = function(_key) {
 	}
 };
 
-/** array of predetermined enemy positions on y */
+/** Array of predetermined enemy positions on y */
 var positionArray = [65, 145, 227]; 
 
 /** 
@@ -90,11 +90,11 @@ var Enemy = function() {
  */
 
 Enemy.prototype.update = function(dt) {
-	/** updates enemy position when enemy leaves canvas boundaries */
+	/** Updates enemy position when enemy leaves canvas boundaries */
 	if (this.x > 505) {
 		this.x = -80;
 		this.y = positionArray[Math.floor(Math.random() * 3)];
-	/** activates enemy instances by incrementing their x positions by a randomly determined speed */
+	/** Activates enemy instances by incrementing their x positions by a randomly determined speed */
 	} else {
 		this.x += (Math.random() * this.speed + 1) * dt;
 	}
@@ -126,7 +126,7 @@ var player = new Player();
 /**
  * Add an event listener that listens for key presses. 
  * Also send the keys to the Player.handleInput() method. 
- * /
+ */
 document.addEventListener('keyup', function(e) {
     var allowedKeys = {
         37: 'left',
